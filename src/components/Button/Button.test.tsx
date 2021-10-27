19c537639ae4c71b6776793ac3be837926dc00ea
HerilS <herilsaha06@gmail.com>
2021-10-26 21:48:24 -0600

    refactor(components/button): refactored unit tests (enzyme)

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,9 +1,9 @@
-import renderer from 'react-test-renderer';
-import { cleanup, fireEvent, render } from '@testing-library/react';
+import { cleanup } from '@testing-library/react';
 import { matchers } from '@emotion/jest';
 import { transparentize } from 'polished';
 import Button from '.';
 import { GlobalTheme } from '@/UI';
+import { mountWithTheme } from '@/utils';
 
 expect.extend(matchers);
 
@@ -13,34 +13,33 @@ describe('components/Button', () => {
   describe('standard', () => {
     it('base functions', () => {
       const mockFn = jest.fn();
-      const { getByText } = render(<Button onClick={mockFn}>Click Me</Button>);
-      const node = getByText('Click Me');
-      fireEvent.click(node);
+      const wrapper = mountWithTheme(<Button onClick={mockFn} />);
+      wrapper.simulate('click');
       expect(mockFn).toHaveBeenCalled();
     });
     it('snapshot testing', () => {
-      const tree = renderer.create(<Button />).toJSON();
-      expect(tree).toMatchSnapshot();
+      const wrapper = mountWithTheme(<Button />);
+      expect(wrapper).toMatchSnapshot();
     });
   });
   describe('color', () => {
     describe('primary button', () => {
       it('correct background and color', () => {
-        const tree = renderer.create(<Button color="primary" />).toJSON();
+        const wrapper = mountWithTheme(<Button color="primary" />);
 
         // correct background
-        expect(tree).toHaveStyleRule(
+        expect(wrapper).toHaveStyleRule(
           'background',
           GlobalTheme.colors.primary[500],
         );
 
         // correct color
-        expect(tree).toHaveStyleRule('color', 'white');
+        expect(wrapper).toHaveStyleRule('color', 'white');
       });
       it('primary &:hover state', () => {
-        const tree = renderer.create(<Button color="primary" />).toJSON();
+        const wrapper = mountWithTheme(<Button color="primary" />);
 
-        expect(tree).toHaveStyleRule(
+        expect(wrapper).toHaveStyleRule(
           'background',
           GlobalTheme.colors.primary[600],
           {
@@ -49,8 +48,8 @@ describe('components/Button', () => {
         );
       });
       it('primary &:active state', () => {
-        const tree = renderer.create(<Button color="primary" />).toJSON();
-        expect(tree).toHaveStyleRule(
+        const wrapper = mountWithTheme(<Button color="primary" />);
+        expect(wrapper).toHaveStyleRule(
           'background',
           GlobalTheme.colors.primary[700],
           {
@@ -62,21 +61,21 @@ describe('components/Button', () => {
 
     describe('secondary button', () => {
       it('correct background and color', () => {
-        const tree = renderer.create(<Button color="secondary" />).toJSON();
+        const wrapper = mountWithTheme(<Button color="secondary" />);
 
         // correct background
-        expect(tree).toHaveStyleRule(
+        expect(wrapper).toHaveStyleRule(
           'background',
           GlobalTheme.colors.secondary[500],
         );
 
         // correct color
-        expect(tree).toHaveStyleRule('color', 'white');
+        expect(wrapper).toHaveStyleRule('color', 'white');
       });
       it('secondary &:hover state', () => {
-        const tree = renderer.create(<Button color="secondary" />).toJSON();
+        const wrapper = mountWithTheme(<Button color="secondary" />);
 
-        expect(tree).toHaveStyleRule(
+        expect(wrapper).toHaveStyleRule(
           'background',
           GlobalTheme.colors.secondary[600],
           {
@@ -85,8 +84,8 @@ describe('components/Button', () => {
         );
       });
       it('secondary &:active state', () => {
-        const tree = renderer.create(<Button color="secondary" />).toJSON();
-        expect(tree).toHaveStyleRule(
+        const wrapper = mountWithTheme(<Button color="secondary" />);
+        expect(wrapper).toHaveStyleRule(
           'background',
           GlobalTheme.colors.secondary[700],
           {
@@ -98,21 +97,21 @@ describe('components/Button', () => {
 
     describe('accent button', () => {
       it('correct background and color', () => {
-        const tree = renderer.create(<Button color="accent" />).toJSON();
+        const wrapper = mountWithTheme(<Button color="accent" />);
 
         // correct background
-        expect(tree).toHaveStyleRule(
+        expect(wrapper).toHaveStyleRule(
           'background',
           GlobalTheme.colors.accent[500],
         );
 
         // correct color
-        expect(tree).toHaveStyleRule('color', 'white');
+        expect(wrapper).toHaveStyleRule('color', 'white');
       });
       it('accent &:hover state', () => {
-        const tree = renderer.create(<Button color="accent" />).toJSON();
+        const wrapper = mountWithTheme(<Button color="accent" />);
 
-        expect(tree).toHaveStyleRule(
+        expect(wrapper).toHaveStyleRule(
           'background',
           GlobalTheme.colors.accent[600],
           {
@@ -121,8 +120,8 @@ describe('components/Button', () => {
         );
       });
       it('accent &:active state', () => {
-        const tree = renderer.create(<Button color="accent" />).toJSON();
-        expect(tree).toHaveStyleRule(
+        const wrapper = mountWithTheme(<Button color="accent" />);
+        expect(wrapper).toHaveStyleRule(
           'background',
           GlobalTheme.colors.accent[700],
           {
@@ -134,21 +133,21 @@ describe('components/Button', () => {
 
     describe('grey button', () => {
       it('correct background and color', () => {
-        const tree = renderer.create(<Button color="grey" />).toJSON();
+        const wrapper = mountWithTheme(<Button color="grey" />);
 
         // correct background
-        expect(tree).toHaveStyleRule(
+        expect(wrapper).toHaveStyleRule(
           'background',
           GlobalTheme.colors.grey[100],
         );
 
         // correct color
-        expect(tree).toHaveStyleRule('color', GlobalTheme.colors.grey[800]);
+        expect(wrapper).toHaveStyleRule('color', GlobalTheme.colors.grey[800]);
       });
       it('grey &:hover state', () => {
-        const tree = renderer.create(<Button color="grey" />).toJSON();
+        const wrapper = mountWithTheme(<Button color="grey" />);
 
-        expect(tree).toHaveStyleRule(
+        expect(wrapper).toHaveStyleRule(
           'background',
           GlobalTheme.colors.grey[200],
           {
@@ -157,8 +156,8 @@ describe('components/Button', () => {
         );
       });
       it('grey &:active state', () => {
-        const tree = renderer.create(<Button color="grey" />).toJSON();
-        expect(tree).toHaveStyleRule(
+        const wrapper = mountWithTheme(<Button color="grey" />);
+        expect(wrapper).toHaveStyleRule(
           'background',
           GlobalTheme.colors.grey[300],
           {
@@ -171,18 +170,18 @@ describe('components/Button', () => {
 
   describe('variants', () => {
     it('solid variant', () => {
-      const tree = renderer.create(<Button variant="solid" />).toJSON();
-      expect(tree).toHaveStyleRule('border', '0');
+      const wrapper = mountWithTheme(<Button variant="solid" />);
+      expect(wrapper).toHaveStyleRule('border', '0');
 
-      expect(tree).toHaveStyleRule(
+      expect(wrapper).toHaveStyleRule(
         'background',
         GlobalTheme.colors.primary[500],
       );
     });
     it('text variant', () => {
-      const tree = renderer.create(<Button variant="text" />).toJSON();
-      expect(tree).toHaveStyleRule('background', 'transparent');
-      expect(tree).toHaveStyleRule(
+      const wrapper = mountWithTheme(<Button variant="text" />);
+      expect(wrapper).toHaveStyleRule('background', 'transparent');
+      expect(wrapper).toHaveStyleRule(
         'background',
         transparentize(0.9, GlobalTheme.colors.primary[500]),
         {
@@ -190,7 +189,7 @@ describe('components/Button', () => {
         },
       );
 
-      expect(tree).toHaveStyleRule(
+      expect(wrapper).toHaveStyleRule(
         'background',
         transparentize(0.7, GlobalTheme.colors.primary[500]),
         {
@@ -198,19 +197,23 @@ describe('components/Button', () => {
         },
       );
 
-      expect(tree).toHaveStyleRule('color', GlobalTheme.colors.primary[600], {
-        target: ':active',
-      });
+      expect(wrapper).toHaveStyleRule(
+        'color',
+        GlobalTheme.colors.primary[600],
+        {
+          target: ':active',
+        },
+      );
     });
 
     it('outlined variant', () => {
-      const tree = renderer.create(<Button variant="outlined" />).toJSON();
+      const wrapper = mountWithTheme(<Button variant="outlined" />);
 
-      expect(tree).toHaveStyleRule('background', 'transparent');
-      expect(tree).toHaveStyleRule('border', GlobalTheme.borders.sm);
-      expect(tree).toHaveStyleRule('color', GlobalTheme.colors.primary[500]);
+      expect(wrapper).toHaveStyleRule('background', 'transparent');
+      expect(wrapper).toHaveStyleRule('border', GlobalTheme.borders.sm);
+      expect(wrapper).toHaveStyleRule('color', GlobalTheme.colors.primary[500]);
 
-      expect(tree).toHaveStyleRule(
+      expect(wrapper).toHaveStyleRule(
         'background',
         transparentize(0.9, GlobalTheme.colors.primary[500]),
         {
@@ -218,7 +221,7 @@ describe('components/Button', () => {
         },
       );
 
-      expect(tree).toHaveStyleRule(
+      expect(wrapper).toHaveStyleRule(
         'background',
         transparentize(0.7, GlobalTheme.colors.primary[500]),
         {
@@ -226,11 +229,15 @@ describe('components/Button', () => {
         },
       );
 
-      expect(tree).toHaveStyleRule('color', GlobalTheme.colors.primary[600], {
-        target: ':active',
-      });
+      expect(wrapper).toHaveStyleRule(
+        'color',
+        GlobalTheme.colors.primary[600],
+        {
+          target: ':active',
+        },
+      );
 
-      expect(tree).toHaveStyleRule(
+      expect(wrapper).toHaveStyleRule(
         'border-color',
         GlobalTheme.colors.primary[600],
         {
@@ -242,36 +249,45 @@ describe('components/Button', () => {
 
   describe('sizes', () => {
     it('icon', () => {
-      const tree = renderer.create(<Button size="icon" />).toJSON();
-      expect(tree).toHaveStyleRule('height', GlobalTheme.sizes.sm);
+      const wrapper = mountWithTheme(<Button size="icon" />);
+      expect(wrapper).toHaveStyleRule('height', GlobalTheme.sizes.sm);
     });
 
     it('sm', () => {
-      const tree = renderer.create(<Button size="sm" />).toJSON();
-      expect(tree).toHaveStyleRule('height', GlobalTheme.sizes.sm);
-      expect(tree).toHaveStyleRule('padding', GlobalTheme.space.xs);
-      expect(tree).toHaveStyleRule('font-size', GlobalTheme.fontSizes.bodySm);
+      const wrapper = mountWithTheme(<Button size="sm" />);
+      expect(wrapper).toHaveStyleRule('height', GlobalTheme.sizes.sm);
+      expect(wrapper).toHaveStyleRule('padding', GlobalTheme.space.xs);
+      expect(wrapper).toHaveStyleRule(
+        'font-size',
+        GlobalTheme.fontSizes.bodySm,
+      );
     });
 
     it('md', () => {
-      const tree = renderer.create(<Button size="md" />).toJSON();
-      expect(tree).toHaveStyleRule('height', GlobalTheme.sizes.md);
-      expect(tree).toHaveStyleRule('padding', GlobalTheme.space.sm);
-      expect(tree).toHaveStyleRule('font-size', GlobalTheme.fontSizes.bodyLg);
+      const wrapper = mountWithTheme(<Button size="md" />);
+      expect(wrapper).toHaveStyleRule('height', GlobalTheme.sizes.md);
+      expect(wrapper).toHaveStyleRule('padding', GlobalTheme.space.sm);
+      expect(wrapper).toHaveStyleRule(
+        'font-size',
+        GlobalTheme.fontSizes.bodyLg,
+      );
     });
 
     it('lg', () => {
-      const tree = renderer.create(<Button size="lg" />).toJSON();
-      expect(tree).toHaveStyleRule('height', GlobalTheme.sizes.lg);
-      expect(tree).toHaveStyleRule('padding', GlobalTheme.space.md);
-      expect(tree).toHaveStyleRule('font-size', GlobalTheme.fontSizes.bodyLg);
+      const wrapper = mountWithTheme(<Button size="lg" />);
+      expect(wrapper).toHaveStyleRule('height', GlobalTheme.sizes.lg);
+      expect(wrapper).toHaveStyleRule('padding', GlobalTheme.space.md);
+      expect(wrapper).toHaveStyleRule(
+        'font-size',
+        GlobalTheme.fontSizes.bodyLg,
+      );
     });
   });
 
   describe('maxWidth', () => {
     it('makes width 100%', () => {
-      const tree = renderer.create(<Button maxWidth />).toJSON();
-      expect(tree).toHaveStyleRule('width', '100%');
+      const wrapper = mountWithTheme(<Button maxWidth />);
+      expect(wrapper).toHaveStyleRule('width', '100%');
     });
   });
 });
--- src/utils/mountWithTheme/mountWithTheme.tsx
+++ src/utils/mountWithTheme/mountWithTheme.tsx
@@ -1,8 +1,8 @@
-import { mount, MountRendererProps } from 'enzyme';
+import { mount, ReactWrapper } from 'enzyme';
 import { ThemeProvider } from '@emotion/react';
 import { GlobalTheme } from '@/UI';
 
-const mountWithTheme = (tree: JSX.Element): MountRendererProps => {
+const mountWithTheme = (tree: JSX.Element): ReactWrapper => {
   const WrappingThemeProvider = ({ children }: { children: JSX.Element }) => (
     <ThemeProvider theme={GlobalTheme}>{children}</ThemeProvider>
   );
